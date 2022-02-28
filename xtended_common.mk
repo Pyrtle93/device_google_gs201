26ae811fbe466efd2eba7a407a4196819ed4faac
ReallySnow <reallysnow233@gmail.com>
2022-02-28 08:31:54 +0000

    Add AiAi allowlist
    * In Android 12, Android System Intelligence (com.google.android.as) uses
      some of the Private Compute Service (com.google.android.as.oss) services.
      The absence of these Allowlists will cause Android System Intelligence
      to fail to connect to the network and cause collapse.
    * We don't need to extract google.xml to fix it, just extract the AiAi part
    
    Change-Id: I446ae5d243cff134901094f82c90a0bbf3bb4381

--- xtended_common.mk
+++ xtended_common.mk
@@ -10,3 +10,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-xtended
 PRODUCT_PACKAGES += \
     FaceEnrollOverlay \
     FaceEnrollSettingsOverlay
+
+# AiAi Config
+PRODUCT_COPY_FILES += \
+    device/google/gs201/allowlist_com.google.android.as.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/allowlist_com.google.android.as.xml
